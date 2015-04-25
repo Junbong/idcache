f11c4ccbe3184684493a916a8ad5f9919f456fa9
Junbong <koliie9039@gmail.com>
2015-04-25 23:18:15 +0900

    package-info added.

---
+++ src/main/java/io/idcache/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * The idCache is an policy based identifier generator
+ * with cache library written in Java.
+ * It is good for item store to giving a unique identifier
+ * value to each items. For example, you would like to set
+ * SID-stuff identifier- to each items for classify it.
+ * In this case, idCache is a good choice. It provides you
+ * highly abstracted value generator which take incremental
+ * policy.
+ */
+package io.idcache;
