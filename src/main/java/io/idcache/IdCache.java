23d20ab1780cd03e85ec3da441e45815dc8b607c
Junbong Lee <koliie9039@gmail.com>
2015-04-25 23:10:46 +0900

    Methods * public T getInitial(); * public T getMinimum(); * public T getMaximum(); * public T set(T newValue); * public Increaser<?> getIncreaser(); moved from AbstractNumberIdCache

--- src/main/java/io/idcache/IdCache.java
+++ src/main/java/io/idcache/IdCache.java
@@ -1,11 +1,25 @@
 package io.idcache;
 
 
+import io.idcache.increaser.Increaser;
+
+
+
 /**
  * Created by bong on 15. 4. 23.
  */
 public interface IdCache<T> {
+	public T getInitial();
+
+	public T getMinimum();
+
+	public T getMaximum();
+
 	public T current();
+
 	public T next();
+
 	public T set(T newValue);
+
+	public Increaser<?> getIncreaser();
 }
