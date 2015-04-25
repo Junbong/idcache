147a3d6c87637e25a40117f689a3bd1b5bc20f81
Junbong <koliie9039@gmail.com>
2015-04-25 23:12:14 +0900

    Separate into two implementation * AbstractNumberIdCache * AbstractGenericIdCache

--- src/main/java/io/idcache/AbstractNumberIdCache.java
+++ src/main/java/io/idcache/AbstractNumberIdCache.java
@@ -2,98 +2,15 @@ package io.idcache;
 
 
 import io.idcache.increaser.Increaser;
+import io.idcache.increaser.integer.SequentialIntIncreaser;
 
 
 
 /**
  * Created by bong on 15. 4. 23.
  */
-public abstract class AbstractNumberIdCache<T> implements IdCache<T> {
-	private final Increaser<T> mIncreaser;
-	private final T mInitialValue;
-	private final T mMinimumValue;
-	private final T mMaximumValue;
-
-	private LimitationPolicy mLimitPolicy = LimitationPolicy.ROLL_TO_MINIMUM_POLICY;
-
-
-
-	public AbstractNumberIdCache(T initValue, T minValue, T maxValue, Increaser<T> increaser) {
-		this.mInitialValue = initValue;
-		this.mMinimumValue = minValue;
-		this.mMaximumValue = maxValue;
-		this.mIncreaser = increaser;
-	}
-
-
-
-	public T getInitial() {
-		return this.mInitialValue;
-	}
-
-
-
-	public T getMinimum() {
-		return this.mMinimumValue;
-	}
-
-
-
-	public T getMaximum() {
-		return this.mMaximumValue;
-	}
-
-
-
-	@Override public abstract T current();
-
-
-
-	@Override public final T next() {
-		T value;
-
-		try {
-			value = onNextImpl();
-
-		} catch (LimitationReachedException e) {
-			switch (getLimitationPolicy()) {
-				case INITIALIZE_POLICY:
-					return set(getInitial());
-
-				case ROLL_TO_MINIMUM_POLICY:
-					return set(getMinimum());
-
-				case ROLL_TO_MAXIMUM_POLICY:
-					return set(getMaximum());
-
-				case THROW_EXCEPTION_POLICY:
-				default:
-					throw new LimitationReachedException(e);
-			}
-		}
-
-		return value;
-	}
-
-
-
-	protected abstract T onNextImpl() throws LimitationReachedException;
-
-
-
-	public Increaser<T> getIncreaser() {
-		return this.mIncreaser;
-	}
-
-
-
-	public void setLimitationPolicy(LimitationPolicy policy) {
-		this.mLimitPolicy = policy;
-	}
-
-
-
-	public LimitationPolicy getLimitationPolicy() {
-		return mLimitPolicy;
+public abstract class AbstractNumberIdCache<T extends Number> extends AbstractGenericIdCache<T> {
+	public AbstractNumberIdCache(T initValue, T minValue, T maxValue, Increaser<?> increaser) {
+		super(initValue, minValue, maxValue, (increaser != null) ? increaser : new SequentialIntIncreaser());
 	}
 }
